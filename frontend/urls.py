7a7614850772ff03ca0f390dec729e80f44a0b5d
lisin <lisinnic@gmail.com>
2022-08-22 00:06:49 -0700

    Routes and links correction.

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('offers', views.index),
+    path('help', views.index),
+    path('login', views.index),
+    path('forgot', views.index),
+    path('register', views.index),
+    path('help/legal', views.index),
 ]
